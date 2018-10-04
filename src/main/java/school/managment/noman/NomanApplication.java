cff4a09af854446b436bd4ee13f5ffbd5ff11f9f
Noman Arshad <noman.arshad@soco-engineers.com>
2018-10-04 15:07:59 +0500

    empty setup

---
+++ src/main/java/school/managment/noman/NomanApplication.java
@@ -0,0 +1,12 @@
+package school.managment.noman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomanApplication.class, args);
+	}
+}
